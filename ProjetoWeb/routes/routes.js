3f4be148ac3d2f7ef7e16e8d266486d0bb35f7b5
Kayck Rossi <kayck.12gabriel@gmail.com>
2024-04-02 01:07:53 -0300

    tela de cadastro funcionando e finalizada/Iniciando tela de home

--- ProjetoWeb/routes/routes.js
+++ ProjetoWeb/routes/routes.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const cadastroController = require('../controllers/cadastroController');
 
-// Rota para a página de cadastro
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../view', 'cadastro.html'));
+router.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'view', 'home.html'));
 });
+
+router.get('/cadastro', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'view', 'cadastro.html'));
+});
+
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'view', 'login.html'));
+});
+
+// Rota para lidar com o formulário de cadastro (método POST)
+router.post('/cadastro', cadastroController.cadastrarUsuario);
 
 module.exports = router;
